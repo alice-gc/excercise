7dab67467bcddf7ccefc856141149c42795c2347
alice-gc <alicezolnieruk@gmail.com>
2022-06-27 12:52:19 +0100

    Update 8_06_27_104333_create_jobs_table.php

--- laravel_backend/database/migrations/8_06_27_104333_create_jobs_table.php
+++ laravel_backend/database/migrations/8_06_27_104333_create_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -15,6 +15,39 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedInteger('teacher_profile_id')->nullable();
+            $table->foreign('teacher_profile_id')
+                ->references('id')
+                ->on('teacher_datas')
+                ->onDelete('cascade');
+
+            $table->unsignedInteger('subject_id');
+            $table->foreign('subject_id')
+                ->references('id')
+                ->on('subjects')
+                ->onDelete('cascade');
+
+
+
+            $table->unsignedInteger('keystage_id');
+            $table->foreign('keystage_id')
+                ->references('id')
+                ->on('keystage')
+                ->onDelete('cascade');
+
+
+            $table->unsignedInteger('school_id');
+            $table->foreign('school_id')
+                ->references('id')
+                ->on('schools')
+                ->onDelete('cascade');
+
+            $table->string('date');
+
+            $table->string('time_boundries');
+
+
             $table->timestamps();
         });
     }
