1758d0d0532a7bb91016774fc2f8a2b4ba7c5404
emmxgibbs <88027784+emmxgibbs@users.noreply.github.com>
2022-06-27 14:44:42 +0100

    --

--- laravel_backend/database/migrations/600_create_keystages_table.php
+++ laravel_backend/database/migrations/600_create_keystages_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration 
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('keystages', function (Blueprint $table) {
             $table->increments('id');
-
             $table->string('keystage');
             $table->timestamps();
         });
