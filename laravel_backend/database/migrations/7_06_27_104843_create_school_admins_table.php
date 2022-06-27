5055c98c586fab0f4fc49e99da6aefd7d1163c7b
alice-gc <alicezolnieruk@gmail.com>
2022-06-27 12:45:34 +0100

    update 13:30

--- laravel_backend/database/migrations/7_06_27_104843_create_school_admins_table.php
+++ laravel_backend/database/migrations/7_06_27_104843_create_school_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -15,6 +15,21 @@ return new class extends Migration
     {
         Schema::create('school_admins', function (Blueprint $table) {
             $table->id();
+
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->unsignedInteger('school_id');
+            $table->foreign('school_id')
+                ->references('id')
+                ->on('schools')
+                ->onDelete('cascade');
+
+
             $table->timestamps();
         });
     }
