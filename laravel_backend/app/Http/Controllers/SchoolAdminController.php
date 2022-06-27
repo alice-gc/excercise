983eaa229964bf1c47e31d926391a1d7f5992300
alice-gc <alicezolnieruk@gmail.com>
2022-06-27 12:01:16 +0100

    -mcrs for database setup

---
+++ laravel_backend/app/Http/Controllers/SchoolAdminController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\School_admin;
+use Illuminate\Http\Request;
+
+class SchoolAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\School_admin  $school_admin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(School_admin $school_admin)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\School_admin  $school_admin
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(School_admin $school_admin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\School_admin  $school_admin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, School_admin $school_admin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\School_admin  $school_admin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(School_admin $school_admin)
+    {
+        //
+    }
+}
---
+++ laravel_backend/database/migrations/2022_06_27_104221_create_keystage_teacher_datas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keystage_teacher_datas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('keystage_teacher_datas');
+    }
+};
