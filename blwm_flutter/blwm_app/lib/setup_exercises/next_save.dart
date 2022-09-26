d7c4b6c72c056a6864e1102afdd6c775c95de8a7
alice-gc <alicezolnieruk@gmail.com>
2022-09-26 11:45:07 +0100

    fix

--- blwm_flutter/blwm_app/lib/setup_exercises/next_save.dart
+++ blwm_flutter/blwm_app/lib/setup_exercises/next_save.dart
@@ -28,14 +28,12 @@ class NextSave extends StatelessWidget {
   final VoidCallback selectHandler;
   final String next;
 
-  NextSave(this.selectHandler, this.next);
+  const NextSave(this.selectHandler, this.next);
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 150,
-      child: RaisedButton(
-        color: Colors.blue,
-        textColor: Colors.white,
+      child: ElevatedButton(
         child: Text(next),
         onPressed: selectHandler,
       ),
